c8308f489e5edca1430eb4ed34f956b243042761
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2021-01-30 22:48:40 +0200

    Victory scene

--- Assets/Scripts/FinishReached.cs
+++ Assets/Scripts/FinishReached.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishReached : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class FinishReached : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            
+            SceneManager.LoadScene("Victory", LoadSceneMode.Single);
         }
     }
 }

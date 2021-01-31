a3bf991cc41150bcb53d757f8dcd505c41df4218
Aurimas <aurimas.pupsys@aurimas.pupsys-BAYM>
2021-01-31 12:53:47 +0200

    reload on button click

--- Assets/Scripts/LoseScreen.cs
+++ Assets/Scripts/LoseScreen.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoseScreen : MonoBehaviour
 {
     public void DoALoseThing()
     {
-
+        SceneManager.LoadScene("Scenes/UITestScene");
     }
 }

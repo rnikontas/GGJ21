9d2bbd809382cdb428861f766c1593b31b2effe6
Tautvydas Kubolis <tautvydas.kubolis@unity3d.com>
2021-01-31 02:19:32 +0200

    design changes

--- Assets/Scripts/RotatePickupables.cs
+++ Assets/Scripts/RotatePickupables.cs
@@ -6,14 +6,24 @@ public class RotatePickupables : MonoBehaviour
 {
 
     public float rotateSpeed;
-    // Start is called before the first frame update
+    public float moveSpeed;
+
+    private float maxYPos;
+    private float minYPos;
+
     void Start()
     {
+        maxYPos = transform.position.y + 0.15f;
+        minYPos = transform.position.y - 0.15f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0f, rotateSpeed, 0f, Space.World);
+        transform.Rotate(rotateSpeed * Time.deltaTime, rotateSpeed * Time.deltaTime, rotateSpeed * Time.deltaTime, Space.World);
+        if ((moveSpeed > 0 && transform.position.y >= maxYPos) || (moveSpeed < 0 && transform.position.y <= minYPos))
+        {
+            moveSpeed = -moveSpeed;
+        }
+        transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
     }
 }
